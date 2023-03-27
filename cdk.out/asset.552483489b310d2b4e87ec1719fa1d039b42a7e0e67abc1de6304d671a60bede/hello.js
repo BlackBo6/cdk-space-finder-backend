39eba77c247914c9c1d45d15f8cd721777705854
BlackBo6 <53646875+BlackBo6@users.noreply.github.com>
2023-03-27 10:12:56 +0000

    Delete cdk.out directory

--- cdk.out/asset.552483489b310d2b4e87ec1719fa1d039b42a7e0e67abc1de6304d671a60bede/hello.js
+++
@@ -1,9 +0,0 @@
-
-
-
-module.main = async function (event, context) {
-    return {
-        statusCode: 200,
-        response: "Hello from AWS Lambda"
-    }
-}
--- cdk.out/asset.f45bc83b85ec600c125002abd5466e7608d9a9e72bb784e817d33d7ab80e3c78/hello.js
+++
@@ -1,9 +0,0 @@
-
-
-
-exports.main = async function (event, context) {
-    return {
-        statusCode: 200,
-        response: "Hello from AWS Lambda"
-    }
-}
